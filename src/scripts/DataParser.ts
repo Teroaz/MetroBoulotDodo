fd176854b8455675ad0e6dba3dcb3df9c8ce2c48
UnArtisant <73105273+UnArtisant@users.noreply.github.com>
2022-09-07 13:33:43 +0200

    update : data parsing script

--- src/scripts/DataParser.ts
+++ src/scripts/DataParser.ts
@@ -1,11 +1,10 @@
-import metro from '../metro.txt';
+import {writeFileSync, readFileSync} from "fs";
+import {join} from "path";
 
-export namespace DataParser {
-	
-	export const extractDatas = () => {
-		metro.split("\n").forEach((line) => {
-			console.log(line);
-		})
-	}
-	
-}
+const BASE_PATH = "./" + join("src", "data")
+
+const metro = readFileSync(join(BASE_PATH, "metro.txt"), 'utf8')
+
+metro.split("\n").forEach((line) => {
+    console.log(line);
+})
